78ceb23664786f1fe4076646a8b4bb111c2b8218
Zukaritasu <perezjhonlenno29@gmail.com>
2022-01-18 20:44:51 -0400

    Version en C++

---
+++ cpp/Properties.cpp
@@ -0,0 +1,286 @@
+/* Copyright (C) 2021-2022 Zukaritasu
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "Properties.hpp"
+
+#include <iostream>
+#include <fstream>
+
+#include <cstdlib>
+#include <cstring>
+
+
+
+Properties::~Properties()
+{
+	for (auto elem : data) {
+		elem->~basic_string();
+	}
+}
+
+bool Properties::Contains(const std::string& key)
+{
+	return IndexOf(key) != UINT32_MAX;
+}
+
+size_t Properties::IndexOf(const std::string& key)
+{
+	for (size_t i = 0; i < this->data.size(); i += 2) {
+		if (data[i]->compare(key) == 0) {
+			return i;
+		}
+	}
+	return UINT32_MAX;
+}
+
+bool Properties::IsEmpty(const std::string& key)
+{
+	for (auto c : key) {
+		if (!isblank(c)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+std::string* Properties::Convert(const std::string &value)
+{
+	std::string* str = new std::string;
+	for (size_t i = 0; i < value.size(); i++) {
+		if (value[i] == '\\') {
+			if (i + 1 == value.size())
+				break;
+			switch (value[++i]) {
+			case 't': *str += '\t'; break;
+			case 'f': *str += '\f'; break;
+			case 'r': *str += '\r'; break;
+			case 'n': *str += '\n'; break;
+			}
+			continue;
+		}
+
+		*str += value[i];
+	}
+	return str;
+}
+
+const std::string& Properties::ResolveString(const std::string& value)
+{
+	std::string aux;
+	for (size_t i = 0; i < value.size(); i++) {
+		switch (value[i]) {
+		case '\t': aux += "\\t"; break;
+		case '\f': aux += "\\f"; break;
+		case '\r': aux += "\\r"; break;
+		case '\n': aux += "\\n"; break;
+		default:
+			aux += value[i];
+			break;
+		}
+	}
+	return std::string(aux);
+}
+
+void Properties::AddValue(const std::string &value, bool is_value)
+{
+	if (is_value) {
+		data.push_back(value.size() == 0 ? nullptr : Convert(value));
+	} else {
+		data.push_back(new std::string(value));
+	}
+}
+
+void Properties::AddProperty(const std::string &line)
+{
+	bool space = true;
+	bool cp_key = true;
+
+	std::string temp;
+
+	for (size_t i = 0; i < line.size(); i++) {
+		char c = line[i];
+		if (cp_key && (c == '=' || c == ':')) {
+		_value:
+			if (temp.size() == 0)
+				break;
+			AddValue(temp, false);
+			temp.clear();
+			cp_key = false;
+			continue;
+		}
+
+		if (space && isblank((unsigned char)c)) {
+			if (cp_key && i + 1 < line.size()) {
+				c = line[i + 1];
+				if (!isblank((unsigned char)c) && c != '=' && c != ':') {
+					goto _value;
+				}
+			}
+			continue;
+		}
+
+		if (!cp_key && space) {
+			space = false;
+		}
+
+		temp += c;
+
+		if (!cp_key && i + 1 == line.size()) {
+			std::string aux = temp;
+			for (int i = (int)(aux.size() - 1); i >= 0; i--) {
+				if (!isblank((unsigned char)aux[i]))
+					break;
+				temp = aux.substr(0, i);
+			}
+		}
+	}
+
+	if (cp_key) {
+		if (temp.size() != 0) {
+			AddValue(temp, false);
+			AddValue("", true);
+		}
+	} else {
+		AddValue(temp, true);
+	}
+}
+
+void Properties::RemoveRange(uint32_t begin, uint32_t end)
+{
+	for (uint32_t i = begin; i < end; i++) {
+		if (data[i] != nullptr) {
+			data[i]->~basic_string();
+		}
+	}
+	data.erase(data.begin() + begin, data.begin() + end);
+}
+
+bool Properties::ReadLine(std::string& line, std::ifstream& in)
+{
+	int c;
+	bool begin = true;
+	bool comment = false;
+	line.clear();
+
+	while (true) {
+		if ((c = in.get()) == EOF || c == '\n' || c == '\r') {
+			if (comment && c != EOF) {
+				begin = true;
+				comment = false;
+				continue;
+			}
+			break;
+		}
+
+		if (begin) {
+			if (begin = isblank(c) != 0) {
+				continue;
+			} else if (c == '#' || c == ';') {
+				comment = true;
+			}
+		}
+
+		if (!comment) {
+			line += (char)c;
+		}
+	}
+
+	return c != EOF;
+}
+
+bool Properties::Remove(const std::string &key)
+{
+	size_t index = this->IndexOf(key);
+	if (index != UINT32_MAX) {
+		RemoveRange(index, index + 2);
+		return true;
+	}
+	return false;
+}
+
+bool Properties::Replace(const std::string &key, const std::string &value)
+{
+	size_t index = IndexOf(key);
+	if (index++ != UINT32_MAX) {
+		RemoveRange(index, index + 1);
+		data.insert(data.begin() + index, value.size() == 0 ? nullptr : 
+			new std::string(value));
+	}
+	return false;
+}
+
+bool Properties::Add(const std::string &key, const std::string &value)
+{
+	if (IndexOf(key) == UINT32_MAX && !IsEmpty(key)) {
+		data.push_back(new std::string(key));
+		data.push_back(new std::string(value));
+	}
+	return false;
+}
+
+size_t Properties::Size() const
+{
+	return data.size() == 0 ? 0 : data.size() / 2;
+}
+
+bool Properties::Read(const std::string& filename)
+{
+	std::ifstream in(filename);
+	if (in.is_open()) {
+		std::string line;
+		while (ReadLine(line, in) || line.size() > 0) {
+			AddProperty(line);
+		}
+		bool error = in.fail();
+		in.close();
+		return !error;
+	}
+	return false;
+}
+
+bool Properties::Write(const std::string& filename)
+{
+	std::ofstream out(filename);
+	if (out.is_open()) {
+		for (size_t i = 1; !out.fail() && i < data.size(); i += 2) {
+			out << *(data[i - 1]) << '=';
+			if (data[i] != nullptr) {
+				out << ResolveString(*(data[i]));
+			}
+			if (i + 2 < data.size()) {
+				out << std::endl;
+			}
+		}
+		bool error = out.fail();
+		out.close();
+		return !error;
+	}
+	return false;
+}
+
+void Properties::Print() const
+{
+	for (size_t i = 1; i < data.size(); i += 2) {
+		const char* value = data[i] == nullptr ? "" : data[i]->c_str();
+		std::cout << *(data[i - 1]) << '=' << value << '\n';
+	}
+}
+
+const std::string& Properties::GetValue(const std::string &key)
+{
+	size_t i = IndexOf(key);
+	return (i != UINT32_MAX) ? *(data[i + 1]) : std::string();
+}
---
+++ cpp/Properties.hpp
@@ -0,0 +1,305 @@
+/* Copyright (C) 2021-2022 Zukaritasu
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <cstdint>
+
+class Properties
+{
+protected:
+
+	std::vector<std::string*> data;
+private:
+
+	/**
+	 * @brief La funcion retorna la posicion de donde se encuentra la
+	 * clave 'key'. En el caso que la clave sea una cadena vacia o que
+	 * no exista la clave en el conjunto de propiedades la funcion
+	 * retorna 'false'
+	 * 
+	 * @param key la clave de la cual se obtendra la posicion
+	 * @return size_t (UINT32_MAX) o la posicion
+	 */
+	size_t IndexOf(const std::string& key);
+
+	/**
+	 * @brief Verifica que una cadena no este vacia "tomando en cuenta
+	 * los espacios en blanco" y si lo esta retorna true, de lo contrario
+	 * false. Esta funcion es usada para verificar que la clave no sea
+	 * una cadena vacia 
+	 * 
+	 * @param key    la clave que se analizara su contenido
+	 * @return 
+	 */
+	bool IsEmpty(const std::string& key);
+
+	/**
+	 * @brief Lee una linea de un archivo de propiedades. Los espacios
+	 * "si existen en el inicio de la nueva linea" y los comentarios son
+	 * ignorados. La funcion puede retornar false si se llego al final
+	 * del archivo o ocurrio un error al leer la linea
+	 * 
+	 * @param line   el string donde se guarda la linea leida
+	 * @param in     entrada de lectura de archivo
+	 * @return 
+	 */
+	bool ReadLine(std::string& line, std::ifstream& in);
+
+	/**
+	 * @brief Convierte un caracter interpretado a un caracter de
+	 * control los cuales son '\t', '\f', '\r' y '\n'
+	 * 
+	 * Se puede decir que un caracter interpretado se entiende como por
+	 * ejemplo escribir un salto de linea '\n' en un archivo con un editor
+	 * de texto de esta manera "\n" identicamente pero como tal no es un
+	 * caracter sino que se interpreta como un caracter de salto de linea
+	 * sin ni siquiera serlo, es solo para que sea <visible> de que existe
+	 * tal caracter de control en el valor de la clave
+	 * 
+	 * La funcion recibe un string a convetir
+	 * 
+	 * @param value el string a convetir
+	 * @return std::string* resultado
+	 */
+	std::string* Convert(const std::string &value);
+	
+	/**
+	 * @brief Cambia los caracteres de control '\t', '\f', '\r' y '\n'
+	 * agregando una barra diagonal '\' y luego el caracter que representa
+	 * ese caracter para  que desde el archivo se pueda presenciar los
+	 * caracteres de control.
+	 * 
+	 * @param value el string al que cambiaran los caracteres
+	 * @return const std::string& resultado
+	 */
+	const std::string& ResolveString(const std::string& value);
+
+	/**
+	 * @brief Agrega un valor al conjunto de propiedades. Este valor
+	 * puede ser una clave o el valor de la clave, el parametro 'is_value'
+	 * lo indica. En el caso que sea un valor se debe llamar el metodo
+	 * Properties::Convert  para que cambie los caracteres de control
+	 * interpretados por los no interpretados
+	 * 
+	 * @param value el valor a gregar (puede ser una clave o el valor de
+	 * la clave)
+	 * @param is_value indica si 'value' es una clave o el valor de la
+	 * clave
+	 */
+	void AddValue(const std::string &value, bool is_value = false);
+
+	/**
+	 * @brief Agrega una propiedad en el conjunto de propiedades. Esta
+	 * metodo es llamada desde el metodo Properties::Read para procesar
+	 * la linea del archivo que contiene la propiedad. La clave y el
+	 * valor estan separados por los caracteres '=' o ':' tambien es
+	 * valido un espacio en blanco. La clave puede o no contener un valor,
+	 * en ese caso se asigna 'nullptr' como valor por defecto
+	 * 
+	 * @param line la linea del archivo a procesar
+	 */
+	void AddProperty(const std::string &line);
+
+	/**
+	 * @brief Remueve un bloque de elementos del vector indicando la
+	 * posicion inicial y la final. Antes de remover los strings en ese
+	 * rango se destruyen llamando al destructor y luego es posible
+	 * removerlos del vector
+	 * 
+	 * @param begin 
+	 * @param end 
+	 */
+	void RemoveRange(uint32_t begin, uint32_t end);
+public:
+
+	Properties(const Properties&) = delete;
+	~Properties();
+
+	/**
+	 * @brief Retorna true si la clave existe en el conjunto de
+	 * propiedades. La clave no puede ser una cadena vacia y ademas
+	 * debe existir en el conjunto de propiedades
+	 * 
+	 * @param key La clave a validar si existe
+	 * @return 
+	 */
+	bool Contains(const std::string& key);
+
+	/**
+	 * @brief Remueve una clave y su valor del conjunto de propiedades.
+	 * Si la clave no existe el metodo retorna false
+	 * 
+	 * @param key La clave a remover (y su valor)
+	 * @return
+	 */
+	bool Remove(const std::string& key);
+
+	/**
+	 * @brief Reemplaza el valor de una clave por otro. El valor puede
+	 * ser una cadena. El metodo puede retornar false si la clave no
+	 * existe en el conjunto de propiedades
+	 * 
+	 * @param key La clave
+	 * @param value El valor de la clave (puede ser una cadena vacia)
+	 * @return
+	 */
+	bool Replace(const std::string& key, const std::string& value);
+
+	/**
+	 * @brief Agrega una nueva clave y su valor al conjunto de
+	 * propiedades. Para que el metodo tenga éxito la clave no debe 
+	 * existir en el conjunto de propiedades además la clave no puede
+	 * ser una cadena vacía
+	 * 
+	 * @param key   
+	 * @param value 
+	 * @return 
+	 */
+	bool Add(const std::string& key, const std::string& value);
+
+	/**
+	 * @brief Retorna el tamano o la cantidad de propiedades existentes.
+	 * @return size_t 
+	 */
+	size_t Size() const;
+
+	/**
+	 * @brief Lee un archivo de propiedades .properties. La función lee
+	 * todo el conjunto de propiedades en el archivo.
+	 * 
+	 * Una propiedad está constituida por su clave y su valor como un
+	 * diccionario. Una propiedad puede o no tener un valor especificado,
+	 * como por ejemplo “clave= <vacio>”. Si no se encuentra un separador
+	 * como ‘=’  o ‘:’ y solo se encuentra un espacio entre la clave y el
+	 * valor, ese espacio es considerado un separador entre los dos,
+	 * ejemplo:
+	 * 
+	 * 		“clave <espacio> valor”.
+	 * 
+	 * El carácter '\' seguido de 't', 'f', 'r', o una 'n' es convertido
+	 * a un carácter de control cuando es leído el valor. El metodo puede
+	 * retornar false si no existe el archivo o en el caso que ocurra un
+	 * error en la lectura del archivo
+	 * 
+	 * @param filename El nombre del arhivo .properties
+	 * @return 
+	 */
+	bool Read(const std::string& filename);
+
+	/**
+	 * @brief Escribe un archivo de propiedades. Si el valor contiene
+	 * caracteres de control como ‘\t’, ‘\f’, ‘\r’ o ‘\n’ esos caracteres
+	 * serán "visibles en el archivo" como por ejemplo:
+	 * 
+	 * 		“clave=lunes’\n’martes’\n’\t’’”
+	 * 
+	 * El metodo puede retornar false si ocurrio un error al crear el
+	 * archivo en el caso si ocurre un error al escribir en el archivo
+	 * 
+	 * @param filename El nombre del arhivo .properties
+	 * @return
+	 */
+	bool Write(const std::string& filename);
+
+	/**
+	 * @brief Imprime todo el conjunto de propiedades. El metodo muestra
+	 * la clave y el valor con este formato "clave=valor" y deja un salto
+	 * de linea. Si el valor de la clave es nullptr se muestra con este
+	 * formato "clave="
+	 */
+	void Print() const;
+
+	/**
+	 * @brief Retorna el valor soicitado por la clave del valor. Si la
+	 * clave no existe el metodo retorna una cadena vacio
+	 * 
+	 * @param key La clave del valor
+	 * @return const std::string& El valor
+	 */
+	const std::string& GetValue(const std::string& key);
+
+	/**
+	 * @brief Convierte el valor de la clave en un valor entero 'int'
+	 * usando la funcion 'std::stoi'. Si el valor no se pudo convertir, el
+	 * valor por defecto que retorna este metodo es 0 
+	 * 
+	 * @param key La clave del valor
+	 * @return int 
+	 */
+	int GetInt(const std::string& key) {
+		std::string value = GetValue(key);
+		if (value.size() != 0) { return std::stoi(value); }
+		return 0;
+	}
+
+	/**
+	 * @brief Convierte el valor de la clave en un valor entero
+	 * largo 'int64_t' usando la funcion 'std::stoll'. Si el valor no se
+	 * pudo convertir, el valor por defecto que retorna esta funcion es 0 
+	 * 
+	 * @param key La clave del valor
+	 * @return int64_t 
+	 */
+	int64_t GetInt64(const std::string& key) {
+		std::string value = GetValue(key);
+		if (value.size() != 0) { return std::stoll(value); }
+		return 0;
+	}
+
+	/**
+	 * @brief Convierte el valor de la clave en un valor de tipo 'bool'
+	 * usando como condicion si el valor es "true" o "1". Si el valor es
+	 * diferente a lo mencionado la funcion retorna false
+	 * 
+	 * @param key La clave del valor
+	 * @return bool
+	 */
+	bool GetBool(const std::string& key) {
+		std::string value = GetValue(key);
+		if (value.size() != 0) { 
+			return value.compare("true") == 0 || value[0] == '1';
+		}
+		return false;
+	}
+
+	/**
+	 * @brief Convierte el valor de la clave en un valor de tipo 'double'
+	 * usando la funcion 'std::stod'. Si el valor no se pudo convertir, el
+	 * valor por defecto que retorna esta funcion es 0
+	 * 
+	 * @param key La clave del valor
+	 * @return double 
+	 */
+	double GetDouble(const std::string& key) {
+		std::string value = GetValue(key);
+		if (value.size() != 0) { return std::stod(value); }
+		return 0;
+	}
+
+	/**
+	 * @brief Hace lo mismo que el metodo Properties::GetValue pero en este
+	 * con el uso de un operador []
+	 * 
+	 * @param key  La clave del valor
+	 * @return const std::string& 
+	 */
+	const std::string& operator[](const std::string& key) {
+		return GetValue(key);
+	}
+};
