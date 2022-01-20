182e99410de8cb157978116fb0b248dbfdbec380
Zukaritasu <perezjhonlenno29@gmail.com>
2022-01-20 16:07:39 -0400

    Se agregaron mas funciones para obtener el valor en otros tipos de dato

--- include/fileprop.h
+++ include/fileprop.h
@@ -1,15 +1,15 @@
 /* Copyright (C) 2021-2022 Zukaritasu
-   
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
@@ -22,144 +22,145 @@
 #include <stdint.h>
 
 #ifndef ZPEXPORT
-#	ifdef __ZPROP_LOCAL__
-#		define ZPEXPORT __declspec(dllexport)
-#	else
-#		define ZPEXPORT __declspec(dllimport)
-#	endif
-#endif
-
-#ifndef ZPCALL
-#	define ZPCALL __stdcall
+	#ifdef __ZPROP_LOCAL__
+		#define ZPEXPORT __declspec(dllexport)
+	#else
+		#define ZPEXPORT __declspec(dllimport)
+	#endif
 #endif
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #else
-#	include <stdbool.h>
+#include <stdbool.h>
 #endif
 
 	/* string para version ASCII */
-	typedef const char*    cstr_t;
+	typedef const char *cstr_t;
 	/* string para version UNICODE */
-	typedef const wchar_t* cwstr_t;
-	
+	typedef const wchar_t *cwstr_t;
+
 #ifdef __ZPROP_LOCAL__
 	/* struct reservada para el developer */
 	typedef struct zprop
 	{
 		size_t count;
-		char** data;
+		char **data;
 	} zprop, *pzprop;
 #else
 
-	/* handle del conjunto de propiedades */
-	typedef void* pzprop;
+/* handle del conjunto de propiedades */
+typedef void *pzprop;
 #endif // __ZPROP_LOCAL__
 
 	/* Retorna true si la clave existe en el conjunto de propiedades. Si los
 	   parámetros son inválidos la función retorna false o en el caso que no
 	   exista la clave */
-	ZPEXPORT bool ZPCALL zp_contains(pzprop prop, cstr_t key);
-	
+	ZPEXPORT bool zp_contains(pzprop prop, cstr_t key);
+
 	/* Remueve una clave y su valor del conjunto de propiedades. Si los
 	   parámetros son invalidos o la clave no existe la función retorna false.
 	   Tambien puede retornar false si ocurrió un error al reasignar memoria,
 	   en este caso debe consultar errno */
-	ZPEXPORT bool ZPCALL zp_remove(pzprop prop, cstr_t key);
-	
+	ZPEXPORT bool zp_remove(pzprop prop, cstr_t key);
+
 	/* Reemplaza el valor de una clave por otro. El valor puede ser una cadena
 	   vacía o NULL. La función puede retornar false si los parámetro son
 	   inválidos, la clave no existe o un error al reasignar memoria, para lo
 	   ultimo mencionado debe consultar errno */
-	ZPEXPORT bool ZPCALL zp_replace(pzprop prop, cstr_t key, cstr_t value);
-	
+	ZPEXPORT bool zp_replace(pzprop prop, cstr_t key, cstr_t value);
+
 	/* Agrega una nueva clave y su valor al conjunto de propiedades. Para que
 	   la función tenga éxito la clave no debe existir en el conjunto de
 	   propiedades, los parámetros deben ser validos además la clave no puede
 	   ser una cadena vacía y por último es posible que ocurra un error al
 	   reasignar memoria por lo cual debe consultar errno */
-	ZPEXPORT bool ZPCALL zp_add(pzprop prop, cstr_t key, cstr_t value);
-	
+	ZPEXPORT bool zp_add(pzprop prop, cstr_t key, cstr_t value);
+
 	/* Retorna el tamaño o la cantidad de propiedades existentes. La funcion
 	   puede retornar 0 si el parametro es invalido o no existen propiedades */
-	ZPEXPORT size_t ZPCALL zp_size(pzprop prop);
-	
+	ZPEXPORT size_t zp_size(pzprop prop);
+
 	/* Lee un archivo de propiedades .properties. La función lee todo el
 	   conjunto de propiedades en el archivo.
-	   
+
 	   Una propiedad está constituida por su clave y su valor como un
 	   diccionario. Una propiedad puede o no tener un valor especificado, como
 	   por ejemplo “clave= <vacio>”. Si no se encuentra un separador como ‘=’
 	   o ‘:’ y solo se encuentra un espacio entre la clave y el valor, ese
 	   espacio es considerado un separador entre los dos, ejemplo
-	   
+
 			“clave <espacio> valor”.
-	   
+
 	   El carácter '\' seguido de 't', 'f', 'r', u una 'n' es convertido a un
 	   carácter de control cuando es leído el valor. La función puede retornar
 	   false si los parámetros son inválidos, el archivo no existe, error de
 	   referencia a NULL, ejemplo:
-	   
+
 			pzprop prop = NULL;
 			zp_read(&prop, “archive.properties”);
 	   */
-	ZPEXPORT bool ZPCALL zp_read_a(pzprop* prop, cstr_t filename);
-	ZPEXPORT bool ZPCALL zp_read_w(pzprop* prop, cwstr_t filename);
-	
+	ZPEXPORT bool zp_read_a(pzprop *prop, cstr_t filename);
+	ZPEXPORT bool zp_read_w(pzprop *prop, cwstr_t filename);
+
 	/* Escribe un archivo de propiedades. Si el valor contiene caracteres de
 	   control como ‘\t’, ‘\f’, ‘\r’ o ‘\n’ serán caracteres “visibles en el
 	   archivo” como por ejemplo:
-	   
+
 			“clave=lunes’\n’martes’\n’\t’’”
-	   
+
 	   La función puede retornar false si los parámetros son inválidos o
 	   ocurrió un error en la apertura, escritura del archivo o al reasignar
 	   memoria. Debe consultar errno */
-	ZPEXPORT bool ZPCALL zp_write_a(pzprop prop, cstr_t filename);
-	ZPEXPORT bool ZPCALL zp_write_w(pzprop prop, cwstr_t filename);
+	ZPEXPORT bool zp_write_a(pzprop prop, cstr_t filename);
+	ZPEXPORT bool zp_write_w(pzprop prop, cwstr_t filename);
 
 	/* Retorna el valor de la clave solicitada. La función puede retornar
 	   NULL si los parámetros son inválidos o la clave no existe en el
 	   conjunto de propiedades aunque en tal caso una clave puede tener un
 	   valor NULL, hay que tomar eso en cuenta */
-	ZPEXPORT cstr_t ZPCALL zp_value(pzprop prop, cstr_t key);
+	ZPEXPORT cstr_t zp_value(pzprop prop, cstr_t key);
 
 	/* Las siguientes funciones convierte el valor en el tipo de dato que
 	   retorna cada función. Si el valor es inválido siendo NULL o un valor
 	   que no se puede convertir, la función retorna 0 como valor por defecto
-	   
+
 	   El valor para la funcion 'zp_bool' puede ser "true" o '1' para indicar
 	   un valor true, todo distinto de eso es false */
-	ZPEXPORT int     ZPCALL zp_int   (pzprop prop, cstr_t key);
-	ZPEXPORT int64_t ZPCALL zp_int64 (pzprop prop, cstr_t key);
-	ZPEXPORT bool    ZPCALL zp_bool  (pzprop prop, cstr_t key);
-	ZPEXPORT double  ZPCALL zp_double(pzprop prop, cstr_t key);
+	ZPEXPORT int zp_int(pzprop prop, cstr_t key);
+	ZPEXPORT int64_t zp_int64(pzprop prop, cstr_t key);
+	ZPEXPORT bool zp_bool(pzprop prop, cstr_t key);
+	ZPEXPORT long double zp_longdouble(pzprop prop, cstr_t key);
+	ZPEXPORT unsigned long zp_ulong(pzprop prop, cstr_t key);
+	ZPEXPORT uint64_t zp_uint64(pzprop prop, cstr_t key);
+	ZPEXPORT double zp_double(pzprop prop, cstr_t key);
+
 
 	/* Crea un espacio en memoria para el handle. La funcion puede
 	   retornar false si ocurre un error al solicitar memoria */
-	ZPEXPORT pzprop ZPCALL zp_create(void);
-	
+	ZPEXPORT pzprop zp_create(void);
+
 	/* Libera la memoria usada por el puntero 'pzprop' y el conjunto de
 	   propiedades */
-	ZPEXPORT void ZPCALL zp_free(pzprop prop);
+	ZPEXPORT void zp_free(pzprop prop);
 
 #if defined(__ZPROP_LOCAL__) || defined(_DEBUG)
 
 	/* Imprime el conjunto de propiedades. Esta destinado para
 	   debugear aunque se puede modificar este header para poder usar
 	   esta funcion sin necesidad de estar debugeando */
-	ZPEXPORT void ZPCALL zp_print(pzprop prop);
-	
+	ZPEXPORT void zp_print(pzprop prop);
+
 #endif // defined(__ZPROP_LOCAL__) || defined(_DEBUG)
 
 /* Compatibilidad con ASCII y UNICODE */
 #ifdef UNICODE /* UNICODE */
-#	define zp_read zp_read_w
-#	define zp_write zp_write_w
-#else          /* ASCII */
-#	define zp_read zp_read_a
-#	define zp_write zp_write_a
+	#define zp_read zp_read_w
+	#define zp_write zp_write_w
+#else /* ASCII */
+	#define zp_read zp_read_a
+	#define zp_write zp_write_a
 #endif // UNICODE
 
 #ifdef __cplusplus
